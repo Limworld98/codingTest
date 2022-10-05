57f4ff9fe71753cea341d6d043acb4090c3166d7
Limworld98 <phoenix1228@daum.net>
2022-10-06 00:05:23 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(x: Int): Boolean {
+        var answer = true
+        var sum = 0
+        var temp = x
+        while(temp > 0) {
+            sum += temp % 10
+            temp /= 10
+        }
+
+        return x % sum == 0
+    }
+}
