61179d4b54e57ebe55e6c4487c85c947f83badbe
Limworld98 <phoenix1228@daum.net>
2023-04-29 09:11:48 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,7 +1,8 @@
 -- 코드를 입력하세요
-SELECT i.board_id,i.writer_id,i.title,i.price,(case when i.status = 'sale' then '판매중' 
-                                               when i.status = 'reserved' then '예약중' 
-                                               when i.status = 'done' then '거래완료' end) 
-                                               as status from used_goods_board as i
-where i.created_date like '2022-10-05'
+SELECT i.board_id,i.writer_id,i.title,i.price,(case when i.status = 'SALE' then '판매중'
+                                              when i.status = 'RESERVED' then '예약중'
+                                               when i.status = 'DONE' then '거래완료'
+                                              
+                                               end)as status from used_goods_board i
+where date_format(i.created_date,'%Y-%m-%d') like '2022-10-05'
 order by i.board_id desc;
