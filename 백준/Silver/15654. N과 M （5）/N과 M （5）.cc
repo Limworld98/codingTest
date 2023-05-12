22d98f742d72d8143bf608243e4fccbe1d3c4d14
Limworld98 <phoenix1228@daum.net>
2023-05-13 02:32:52 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void permutation(vector<int> &mainVec, vector<bool> &checkVec, vector<int>& numVec, int depth , int n, int r) {
+    if (depth == r) {
+        for (int i = 0; i < r; i++)
+            cout << mainVec[i] << " ";
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (!checkVec[i]) {
+            checkVec[i] = true;
+            mainVec[depth] = numVec[i];
+            permutation(mainVec, checkVec, numVec , depth + 1 ,  n , r);
+            checkVec[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n,m; 
+    int temp;
+    cin >> n >> m;
+
+    vector<int> numVec;
+
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        numVec.push_back(temp);
+    }
+    vector<int> mainVec(n, 0);
+    vector<bool> checkVec(n+1,false);
+    
+    sort(numVec.begin(), numVec.end());
+    permutation(mainVec, checkVec , numVec, 0 , n , m);
+}
